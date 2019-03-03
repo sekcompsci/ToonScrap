811f8f1e6b93806df4d9aeb623b95f01729aea75
Sirawit Moonrinta <sirawit_m@cmu.ac.th>
2019-03-04 01:23:56 +0700

    refactor

--- backend/app/router.js
+++
@@ -1,9 +0,0 @@
-// Import dependency //
-import router from 'koa-route';
-
-// Import module //
-import { getCartoonList } from './action';
-
-router.get('/', getCartoonList);
-
-export default router;
--- backend/app/server.js
+++ backend/app/server.js
@@ -1,6 +1,5 @@
 // Import dependency //
 import koa from 'koa';
-import koaBody from 'koa-body';
 import router from 'koa-route';
 import config from 'config';
 
@@ -11,14 +10,12 @@ import { getAll, getWithTitle, getWithTitleAndChapter, getWithTitleAndChapterNum
 const app = new koa();
 const port = process.env.PORT || config.get('port');
 
-// Use middleware //
-app.use(koaBody({ multipart: true }));
-
 // Router //
-app.use(router.get('/api/all', getAll));
-app.use(router.get('/api/:name', getWithTitle));
-app.use(router.get('/api/:name/:chapter', getWithTitleAndChapter));
-app.use(router.get('/api/:name/:chapter/:num', getWithTitleAndChapterNum));
+app
+	.use(router.get('/api/all', getAll))
+	.use(router.get('/api/:name', getWithTitle))
+	.use(router.get('/api/:name/:chapter', getWithTitleAndChapter))
+	.use(router.get('/api/:name/:chapter/:num', getWithTitleAndChapterNum));
 
 // Running //
 app.listen(port, () => {
