6666e71d1df780706ef6ba6485d0b912c42e71b3
Sirawit Moonrinta <sirawit_m@cmu.ac.th>
2019-04-08 23:56:06 +0700

    fix conflicts

---
+++ frontend/src/Components/Main/Main.js
@@ -0,0 +1,60 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import { Row, Col, Card, Icon, Spin, BackTop } from 'antd';
+import { Link } from 'react-router-dom';
+import axios from '../../utils/axios';
+
+const { Meta } = Card;
+
+const renderLoading = (
+	<div className="spin-container">
+		<Spin />
+	</div>
+);
+
+const Main = () => {
+	const [ cartoons, setCartoons ] = useState([]);
+	const [ loading, setLoading ] = useState(true);
+
+	const getCartoons = async () => {
+		try {
+			let { data } = await axios.get('/all');
+
+			setLoading(false);
+			return setCartoons(data.data);
+		} catch (error) {
+			setLoading(false);
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		getCartoons();
+	}, []);
+
+	return (
+		<Fragment>
+			<BackTop />
+			<h1>อัพเดตล่าสุด</h1>
+			<Row gutter={16}>
+				{loading && renderLoading}
+				{cartoons.map((cartoon, index) => (
+					<Col key={`col-${index}`} span={12} style={{ marginBottom: 16 }}>
+						<Card
+							className="card-cartoon"
+							cover={
+								<Link to={`/${cartoon.link}`}>
+									<img alt={cartoon.name} src={cartoon.img} />
+								</Link>
+							}
+							actions={[ <Icon type="star" />, <Icon type="share-alt" /> ]}
+						>
+							<Meta title={<Link to={`/${cartoon.link}`}>{cartoon.name}</Link>} />
+						</Card>
+					</Col>
+				))}
+			</Row>
+		</Fragment>
+	);
+};
+
+export default Main;
---
+++ frontend/src/Router.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from './components/Main';
+import Name from './components/Name';
+import Chapter from './components/Chapter';
+
+// Route Components
+// use component with path url
+const Router = () => (
+	<BrowserRouter>
+		<Switch>
+			<Route exact path="/" component={Main} />
+			<Route exact path="/:name" component={Name} />
+			<Route exact path="/:name/:chapter" component={Chapter} />
+		</Switch>
+	</BrowserRouter>
+);
+
+export default Router;
