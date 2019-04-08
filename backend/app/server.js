961792a23988aa5d0922b9b36cf7ec5429fc33ef
Sirawit Moonrinta <sirawit_m@cmu.ac.th>
2019-04-09 00:00:07 +0700

    fix conflicts

---
+++ backend/app/server.js
@@ -0,0 +1,27 @@
+// Import dependency //
+import koa from 'koa';
+import router from 'koa-route';
+import cors from '@koa/cors';
+import config from 'config';
+
+// Import module //
+import { getAll, getWithTitle, getWithTitleAndChapter, getWithTitleAndChapterNum } from './action';
+
+// Define Varriable //
+const app = new koa();
+const port = process.env.PORT || config.get('port');
+
+// Router //
+app
+	.use(cors()) // use middleware
+	.use(router.get('/api/all', getAll))
+	.use(router.get('/api/:name', getWithTitle))
+	.use(router.get('/api/:name/:chapter', getWithTitleAndChapter))
+	.use(router.get('/api/:name/:chapter/:num', getWithTitleAndChapterNum));
+
+// Running //
+app.listen(port, () => {
+	console.log('Starting node.js on port ' + port);
+});
+
+export default app;
