2d77647f6bf2694fb2681db1e4a7e68775dd5b60
Sirawit Moonrinta <sirawit_m@cmu.ac.th>
2019-04-11 12:55:08 +0700

    update cartoon cover size

--- backend/app/action.js
+++ backend/app/action.js
@@ -28,7 +28,7 @@ const getAll = async (ctx) => {
 				let name = $(this).find('.ttl').attr('title');
 				let link = $(this).find('.ttl').attr('href');
 
-				img = img.replace(/36/, 200);
+				img = img.replace(/36/, 350);
 				link = link.split('/');
 
 				cartoon.push({ name, img, link: link[3] });
